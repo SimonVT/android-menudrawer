4f3577e246bdd764cfa3e36ddc9e15271f505daf
Kevin Sawicki <kevin@github.com>
2012-10-15 11:25:45 -0700

    Add missing Override annotation

--- library/src/net/simonvt/widget/SmoothInterpolator.java
+++ library/src/net/simonvt/widget/SmoothInterpolator.java
@@ -4,6 +4,7 @@ import android.view.animation.Interpolator;
 
 public class SmoothInterpolator implements Interpolator {
 
+    @Override
     public float getInterpolation(float t) {
         t -= 1.0f;
         return t * t * t * t * t + 1.0f;
