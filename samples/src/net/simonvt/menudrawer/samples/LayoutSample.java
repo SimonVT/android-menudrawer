08ea5f38d8e80bdf94ec5f5c8fc4563dde2f65ca
Simon Vig Therkildsen <simonvt@gmail.com>
2013-01-27 20:14:22 +0100

    Make drawer items clickable in xml sample.

--- samples/src/net/simonvt/menudrawer/samples/LayoutSample.java
+++ samples/src/net/simonvt/menudrawer/samples/LayoutSample.java
@@ -4,16 +4,33 @@ import net.simonvt.menudrawer.MenuDrawer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LayoutSample extends Activity {
+public class LayoutSample extends Activity implements View.OnClickListener {
 
-    private static final String TAG = "LayoutSample";
+    private MenuDrawer mDrawer;
+    private TextView mContentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layoutsample);
-        MenuDrawer md = (MenuDrawer) findViewById(R.id.drawer);
-        md.setTouchMode(MenuDrawer.TOUCH_MODE_FULLSCREEN);
+        mDrawer = (MenuDrawer) findViewById(R.id.drawer);
+        mDrawer.setTouchMode(MenuDrawer.TOUCH_MODE_FULLSCREEN);
+
+        mContentTextView = (TextView) findViewById(R.id.contentText);
+        findViewById(R.id.item1).setOnClickListener(this);
+        findViewById(R.id.item2).setOnClickListener(this);
+    }
+
+    /**
+     * Click handler for top drawer items.
+     */
+    @Override
+    public void onClick(View v) {
+        String tag = (String) v.getTag();
+        mContentTextView.setText(String.format("%s clicked.", tag));
+        mDrawer.setActiveView(v);
     }
 }
